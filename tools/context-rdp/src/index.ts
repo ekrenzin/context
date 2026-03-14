51e52cab6e9353ca290e13321929bc892dad62ae
Ean Krenzin-Blank <ean@eankrenzin.com>
2026-03-14 13:59:27 -0500

    auto-save 2026-03-14 18:59:27

---
+++ tools/context-rdp/src/index.ts
@@ -0,0 +1,3 @@
+export { parseRdpConfig, type RdpConfig } from "./parse-rdp.js";
+export { ensureGuacd, stopGuacd } from "./guacd.js";
+export { createGuacTunnel, type GuacTunnelOptions, type GuacTunnel } from "./tunnel.js";
